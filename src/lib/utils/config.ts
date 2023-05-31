d937c4acf79833f86fe03ceed1d067726e24c266
AmirSolt <soltamir194@gmail.com>
2023-05-31 07:29:58 -0400

    improved instructs

--- src/lib/utils/config.ts
+++ src/lib/utils/config.ts
@@ -15,14 +15,16 @@ export const ChatGPTTemprature = 0.15 // lower more coherence, higher more creat
 export const ChatGPTInstructions=`
 Follow these instructions:
 - Create a comparison table with given products.
-- Only extract information that describes the product  objectively.
+- Only extract information that describes the product objectively.
+- Every column has to be related to both products.
 - Examples of comparable features are: texture, components, washability so on.
-- Come up with 15 features to compare
+- Come up with 7 features to compare
 - don't use title, asin or brand as a feature.
 - Use "t" or "f" for boolean.
 - Use | to split each cell
 - Every product must have same number of rows.
-- Come up with fifteen features to compare.
+- Come up with atleast seven features to compare.
+- Every column must be related to both products.
 - Only answer in csv.
 - Format must be csv, example response:
          asin|[feature_name]|[feature_name]|...
