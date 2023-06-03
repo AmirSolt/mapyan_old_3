3966f9634822c13e8b58301a2e8197be8bf05c5b
AmirSolt <soltamir194@gmail.com>
2023-06-03 06:29:25 -0400

    made changes to vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			edge:true,
-			// maxDuration: 60,
+			maxDuration: 60,
 			regions: ['iad1'],
 		})
 	}
